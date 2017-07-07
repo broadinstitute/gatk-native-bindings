433b1dd5b374dcb7bc7aaf0f71c10f4c7d4bcd19
pnvaidya <priya.n.vaidya@intel.com>
2017-07-07 14:38:57 -0400

    Add Smith-Waterman native bindings  (#11)
    Adding native bindings interface for Smith-Waterman

---
+++ src/main/java/org/broadinstitute/gatk/nativebindings/smithwaterman/SWAlignerNativeBinding.java
@@ -0,0 +1,34 @@
+package org.broadinstitute.gatk.nativebindings.smithwaterman;
+
+
+import org.broadinstitute.gatk.nativebindings.NativeLibrary;
+import org.broadinstitute.gatk.nativebindings.smithwaterman.SWAlignerNativeArguments;
+import org.broadinstitute.gatk.nativebindings.smithwaterman.SWAlignmentResult;
+
+/**
+ * Provides access to the native kernel of the Smith-Waterman computation.
+ * GATK will call {@link #initialize(SWAlignerNativeArguments)} to set the
+ * parameter values and will call {@link #align(byte[], byte[])} to perform
+ * an alignment.
+ *
+ * Only one thread will call methods on any given object of classes that implement this interface.
+ */
+public interface SWAlignerNativeBinding extends NativeLibrary {
+
+    /**
+     * Initialize the native Smith-Waterman
+     *
+     * @param args arguments used for Smith-Waterman
+     */
+    void initialize(SWAlignerNativeArguments args);
+
+    /**
+     * Perform a Smith-Waterman alignment and return the result
+     *
+     * @param ref reference sequence
+     * @param alt alternate sequence
+     * @return alignment result
+     */
+    SWAlignmentResult align(byte[] ref, byte[] alt);
+
+}
---
+++ src/main/java/org/broadinstitute/gatk/nativebindings/smithwaterman/SWAlignmentResult.java
@@ -0,0 +1,19 @@
+package org.broadinstitute.gatk.nativebindings.smithwaterman;
+
+/**
+ * Struct used to hold results of the Smith-Waterman alignment result
+ */
+public class SWAlignmentResult {
+    // CIGAR string of the alignment
+    public final String cigar;
+
+    // offset of the alignment
+    public final int alignment_offset;
+
+
+    public SWAlignmentResult(final String cigar, final int alignment_offset)
+    {
+        this.cigar = cigar;
+        this.alignment_offset = alignment_offset;
+    }
+}
