78e8f3e910a77b07b63dcbd8ee386a43080b1874
chirayuharyan <chirayuharyan@gmail.com>
2025-03-21 06:37:30 -0700

    Updated AVX levels and added OMP enable/disable option

--- src/main/java/org/broadinstitute/gatk/nativebindings/pdhmm/PDHMMNativeArguments.java
+++ src/main/java/org/broadinstitute/gatk/nativebindings/pdhmm/PDHMMNativeArguments.java
@@ -9,13 +9,23 @@ public class PDHMMNativeArguments {
      */
     public int maxNumberOfThreads;
 
-     /**
+    /**
      * Enum representing the AVX level.
      */
     public enum AVXLevel {
+        FASTEST_AVAILABLE,
         SCALAR,
         AVX2,
         AVX512
+    }
+
+    /**
+     * Enum representing OpenMP settings.
+     */
+    public enum OpenMPSetting {
+        FASTEST_AVAILABLE, // Use OpenMP if available, otherwise fallback to non-OMP
+        ENABLE, // Require OpenMP; fail if not available
+        DISABLE // Disable OpenMP explicitly
     }
 
     /**
@@ -24,8 +34,35 @@ public class PDHMMNativeArguments {
     public AVXLevel avxLevel;
 
     /**
+     * The OpenMP setting to use in the PDHMM computation.
+     */
+    public OpenMPSetting openMPSetting = OpenMPSetting.FASTEST_AVAILABLE; // Default
+
+    /**
      * The maximum memory to use in the PDHMM computation (in megabytes).
      */
-    public int maxMemoryInMB;
+    private int maxMemoryInMB;
+
+    /**
+     * Sets the maximum memory, ensuring it is a positive integer.
+     *
+     * @param maxMemoryInMB the maximum memory to use (in megabytes)
+     * @throws IllegalArgumentException if maxMemoryInMB is not positive
+     */
+    public void setMaxMemoryInMB(int maxMemoryInMB) {
+        if (maxMemoryInMB <= 0) {
+            throw new IllegalArgumentException("maxMemoryInMB must be a positive integer.");
+        }
+        this.maxMemoryInMB = maxMemoryInMB;
+    }
+
+    /**
+     * Gets the maximum memory value.
+     *
+     * @return maxMemoryInMB
+     */
+    public int getMaxMemoryInMB() {
+        return maxMemoryInMB;
+    }
 
 }
